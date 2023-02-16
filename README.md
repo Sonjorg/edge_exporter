cc99d57cb6bfb2034e8908a4f43f5a4b1d45aec6
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-02-16 17:10:44 -0500

    Learning and adding xml attr

--- README.md
+++ README.md
@@ -3,16 +3,19 @@
 ## Following a similar approach as this tutorial:
 https://www.youtube.com/watch?v=9TJx7QTrTyo&t=712s
 
-### Use docker compose up -d in the current dir
+Use
+``` docker compose up -d ```
+in the current dir
 
 ## test to see if it works:
 ### get ip address of grafana container
-sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' grafana
+``` sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' grafana ```
 
 curl ip-address:3000
 
 ### Restart all containers if changes are made to docker-compose.yml
-docker-compose up -d --force-recreate
+``` docker-compose up -d --force-recreate ```
 
 ### check status in log files for a container
-sudo docker-compose logs -f container-name
+```sudo docker-compose logs -f container-name ```
+### Download golang using the official download page and remember to reboot
--- SBCexporter/main.go
+++ SBCexporter/main.go
@@ -4,29 +4,85 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
+	//"bytes"
+	//"flag"
+	//"io"
+	//"log"
+	//"net/http"
+	//"regexp"
+	//"strconv"
+	//"time"
+	//"github.com/hpcloud/tail"
+	//"github.com/prometheus/client_golang/prometheus"
+	//"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type status struct {
-    HTTPcode    int    `xml:"http_code"`
+	HTTPcode int `xml:"http_code"`
 }
 
 type SBCdata struct {
-	XMLname    xml.Name  `xml:"root"`
-	Status     status    `xml:"status"`
+	XMLname xml.Name `xml:"root"`
+	Status  status   `xml:"status"`
+	Isdnsg  isdnsg   `xml:"isdnsg"`
 	//HTTPcode 				int      `xml:"status>http_code"`
 	//Isdnsg					xml.Name `xml:"isdnsg"`
 	//ActionsetTableNumber	int		 `xml:"isdnsg>ActionsetTableNumber"`
 	//
 }
 
+type isdnsg struct {
+	Href   string `xml:"href,attr"`
+	Id     string `xml:"id,attr"`
+}
+
+
+
+/*
+	func NewMetrics(reg prometheus.Registerer) *metrics {
+		m := &metrics{
+			size: prometheus.NewCounter(prometheus.CounterOpts{
+				Namespace: "nginx",
+				Name:      "size_bytes_total",
+				Help:      "Total bytes sent to the clients.",
+			}),
+			/*requests: prometheus.NewCounterVec(prometheus.CounterOpts{
+				Namespace: "nginx",
+				Name:      "http_requests_total",
+				Help:      "Total number of requests.",
+			}, []string{"status_code", "method", "path"}),
+			duration: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+				Namespace: "nginx",
+				Name:      "http_request_duration_seconds",
+				Help:      "Duration of the request.",
+				// Optionally configure time buckets
+				// Buckets:   prometheus.LinearBuckets(0.01, 0.05, 20),
+				Buckets: prometheus.DefBuckets,
+			}, []string{"status_code", "method", "path"}),
+		}
+		reg.MustRegister(m.size)
+		return m
+	}
+*/
 func main() {
+
+	/* var (
+		targetHost = flag.String("target.host", "localhost", "nginx address with basic_status page")
+		targetPort = flag.Int("target.port", 8080, "nginx port with basic_status page")
+		targetPath = flag.String("target.path", "/status", "URL path to scrap metrics")
+		promPort   = flag.Int("prom.port", 9150, "port to expose prometheus metrics")
+		logPath    = flag.String("target.log", "/var/log/nginx/access.log", "path to access.log")
+	)
+	flag.Parse() */
+
 	data, _ := ioutil.ReadFile("APIoutput.xml")
 
 	sbc := &SBCdata{}
 
 	xml.Unmarshal([]byte(data), &sbc)
 
-	fmt.Println(sbc.Status)
+	fmt.Println("SBC router ID: ",sbc.Isdnsg.Id, "\nRouter href: ",sbc.Isdnsg.Href)
+	//fmt.Println()
 	//fmt.Println(note.From)
 	//fmt.Println(note.Heading)
 	//fmt.Println(note.Body)
