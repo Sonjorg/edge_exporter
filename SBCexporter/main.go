0f1469f6a10955c1a2f4308c70cf12ac1ab92902
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-02-20 16:47:34 -0500

    Added labels to data to differentiate routers

--- SBCexporter/main.go
+++ SBCexporter/main.go
@@ -6,7 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
-	"strconv"
+
+	//"strconv"
 	"log"
 	//"bytes"
 	//"flag"
@@ -23,13 +24,14 @@ import (
 )
 
 type status struct {
-	HTTPcode int `xml:"http_code"`
+	HTTPcode string `xml:"http_code"`
 }
 
 type SBCdata struct {
 	XMLname xml.Name `xml:"root"`
 	Status  status   `xml:"status"`
 	Isdnsg  isdnsg   `xml:"isdnsg"`
+
 	//HTTPcode 				int      `xml:"status>http_code"`
 	//Isdnsg					xml.Name `xml:"isdnsg"`
 	//ActionsetTableNumber	int		 `xml:"isdnsg>ActionsetTableNumber"`
@@ -37,77 +39,80 @@ type SBCdata struct {
 }
 
 type isdnsg struct {
-	Href                  string  `xml:"href,attr"`
-	Id                    string  `xml:"id,attr"`
-	IncomingCallattempts  int     `xml:"rt_IncomingCallattempts"`
-	IncomingCallaccepts   int     `xml:"rt_IncomingCallaccepts"`
+	Href                 string `xml:"href,attr"`
+	Id                   string `xml:"id,attr"`
+	IncomingCallattempts int    `xml:"rt_IncomingCallattempts"`
+	IncomingCallaccepts  int    `xml:"rt_IncomingCallaccepts"`
 }
 
-type fooCollector struct {
+type metrics struct {
 	//size     prometheus.Counter
 	IncomingCallattempts *prometheus.Desc
-	IncomingCallaccepts *prometheus.Desc
+	IncomingCallaccepts  *prometheus.Desc
 	//requests *prometheus.CounterVec
 }
 
-func newFooCollector() *fooCollector {
-	return &fooCollector{
+func newFooCollector() *metrics {
+	return &metrics{
 		IncomingCallattempts: prometheus.NewDesc("incoming_call_attempts",
 			"Shows whether a foo has occurred in our cluster",
-			nil, nil,
+			[]string{"Id", "Href"}, nil,
 		),
 		IncomingCallaccepts: prometheus.NewDesc("incoming_call_accepts",
 			"Shows whether a bar has occurred in our cluster",
-			nil, nil,
+			[]string{"Id", "Href"}, nil,
 		),
 	}
 }
 
-	/*func NewConstHistogram(
-		desc *Desc,
-		count uint64,
-		sum float64,
-		buckets map[float64]uint64,
-		labelValues ...string,
-	) (Metric, error)*/
+/*func NewConstHistogram(
+	desc *Desc,
+	count uint64,
+	sum float64,
+	buckets map[float64]uint64,
+	labelValues ...string,
+) (Metric, error)*/
 
-	//Each and every collector must implement the Describe function.
-//It essentially writes all descriptors to the prometheus desc channel.
-	func (collector *fooCollector) Describe(ch chan<- *prometheus.Desc) {
+// Each and every collector must implement the Describe function.
+// It essentially writes all descriptors to the prometheus desc channel.
+func (collector *metrics) Describe(ch chan<- *prometheus.Desc) {
 
-		//Update this section with the each metric you create for a given collector
-		ch <- collector.IncomingCallaccepts
-		ch <- collector.IncomingCallattempts
-	}
-	//Collect implements required collect function for all promehteus collectors
+	//Update this section with the each metric you create for a given collector
+	ch <- collector.IncomingCallaccepts
+	ch <- collector.IncomingCallattempts
+}
 
-	func (collector *fooCollector) Collect(ch chan<- prometheus.Metric) {
+//Collect implements required collect function for all promehteus collectors
 
-		//Implement logic here to determine proper metric value to return to prometheus
-		//for each descriptor or call other functions that do so.
-		var metricValue1 float64
+func (collector *metrics) Collect(ch chan<- prometheus.Metric) {
 
-		data, _ := ioutil.ReadFile("APIoutput.xml")
-		sbc := &SBCdata{}
-		xml.Unmarshal([]byte(data), &sbc)
-		fmt.Println("SBC router ID: ",sbc.Isdnsg.Id, "\nRouter href: ",sbc.Isdnsg.Href)
-		//var metricValue2 float64
-		
-		if s, err := strconv.ParseFloat(sbc.Isdnsg.Id, 64); err == nil {
-			metricValue1 = s
-			fmt.Println(s) // 3.1415927410125732
-		}
-		
+	//Implement logic here to determine proper metric value to return to prometheus
+	//for each descriptor or call other functions that do so.
+	var metricValue1 float64
+	var metricValue2 float64
+	//var HTTPcode float64
 
-		//Write latest value for each metric in the prometheus metric channel.
-		//Note that you can pass CounterValue, GaugeValue, or UntypedValue types here.
-		m1 := prometheus.MustNewConstMetric(collector.IncomingCallattempts, prometheus.GaugeValue, metricValue1)
-		m2 := prometheus.MustNewConstMetric(collector.IncomingCallaccepts, prometheus.GaugeValue, metricValue1)
-		m1 = prometheus.NewMetricWithTimestamp(time.Now().Add(-time.Hour), m1)
-		m2 = prometheus.NewMetricWithTimestamp(time.Now(), m2)
-		ch <- m1
-		ch <- m2
-	}
+	data, _ := ioutil.ReadFile("APIoutput.xml")
+	sbc := &SBCdata{}
+	xml.Unmarshal([]byte(data), &sbc)
+	fmt.Println("Incoming call attempts/accepts: ", sbc.Isdnsg.IncomingCallattempts, "/", sbc.Isdnsg.IncomingCallaccepts, "\nSBC router ID: ", sbc.Isdnsg.Id, "\nRouter href: ", sbc.Isdnsg.Href)
+
+	/*if s, err := strconv.ParseFloat(sbc.Status.HTTPcode, 64); err == nil {
+		HTTPcode = s
+		fmt.Println(s) // 3.1415927410125732
+	}*/
+	//HTTPcode = float64(sbc.Status.HTTPcode)
+	metricValue1 = float64(sbc.Isdnsg.IncomingCallattempts)
+	metricValue2 = float64(sbc.Isdnsg.IncomingCallaccepts)
+	//Write latest value for each metric in the prometheus metric channel.
+	//Note that you can pass CounterValue, GaugeValue, or UntypedValue types here.
+	m1 := prometheus.MustNewConstMetric(collector.IncomingCallattempts, prometheus.GaugeValue, metricValue1, sbc.Isdnsg.Id, sbc.Isdnsg.Href)
+	m2 := prometheus.MustNewConstMetric(collector.IncomingCallaccepts, prometheus.GaugeValue, metricValue2, sbc.Isdnsg.Id, sbc.Isdnsg.Href)
+	m1 = prometheus.NewMetricWithTimestamp(time.Now().Add(-time.Hour), m1)
+	m2 = prometheus.NewMetricWithTimestamp(time.Now(), m2)
+	ch <- m1
+	ch <- m2
+}
 
 func main() {
 
@@ -116,5 +121,5 @@ func main() {
 
 	http.Handle("/console/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":9101", nil))
-	
+
 }
