d07c366a2dc14052b3ed2d531554531f82743dc5
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-02-20 13:33:52 -0500

    first proof concept scraping data and sending to prometheus

--- SBCexporter/main.go
+++ SBCexporter/main.go
@@ -4,6 +4,10 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"time"
+	"strconv"
+	"log"
 	//"bytes"
 	//"flag"
 	//"io"
@@ -13,8 +17,9 @@ import (
 	//"strconv"
 	//"time"
 	//"github.com/hpcloud/tail"
-	//"github.com/prometheus/client_golang/prometheus"
-	//"github.com/prometheus/client_golang/prometheus/promhttp"
+	//exporter "https://github.com/Sonjorg/HDOmonitoring"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type status struct {
@@ -32,58 +37,84 @@ type SBCdata struct {
 }
 
 type isdnsg struct {
-	Href   string `xml:"href,attr"`
-	Id     string `xml:"id,attr"`
+	Href                  string  `xml:"href,attr"`
+	Id                    string  `xml:"id,attr"`
+	IncomingCallattempts  int     `xml:"rt_IncomingCallattempts"`
+	IncomingCallaccepts   int     `xml:"rt_IncomingCallaccepts"`
 }
 
+type fooCollector struct {
+	//size     prometheus.Counter
+	IncomingCallattempts *prometheus.Desc
+	IncomingCallaccepts *prometheus.Desc
+	//requests *prometheus.CounterVec
+}
 
-
-/*
-	func NewMetrics(reg prometheus.Registerer) *metrics {
-		m := &metrics{
-			size: prometheus.NewCounter(prometheus.CounterOpts{
-				Namespace: "nginx",
-				Name:      "size_bytes_total",
-				Help:      "Total bytes sent to the clients.",
-			}),
-			/*requests: prometheus.NewCounterVec(prometheus.CounterOpts{
-				Namespace: "nginx",
-				Name:      "http_requests_total",
-				Help:      "Total number of requests.",
-			}, []string{"status_code", "method", "path"}),
-			duration: prometheus.NewHistogramVec(prometheus.HistogramOpts{
-				Namespace: "nginx",
-				Name:      "http_request_duration_seconds",
-				Help:      "Duration of the request.",
-				// Optionally configure time buckets
-				// Buckets:   prometheus.LinearBuckets(0.01, 0.05, 20),
-				Buckets: prometheus.DefBuckets,
-			}, []string{"status_code", "method", "path"}),
-		}
-		reg.MustRegister(m.size)
-		return m
+func newFooCollector() *fooCollector {
+	return &fooCollector{
+		IncomingCallattempts: prometheus.NewDesc("incoming_call_attempts",
+			"Shows whether a foo has occurred in our cluster",
+			nil, nil,
+		),
+		IncomingCallaccepts: prometheus.NewDesc("incoming_call_accepts",
+			"Shows whether a bar has occurred in our cluster",
+			nil, nil,
+		),
 	}
-*/
+}
+
+	/*func NewConstHistogram(
+		desc *Desc,
+		count uint64,
+		sum float64,
+		buckets map[float64]uint64,
+		labelValues ...string,
+	) (Metric, error)*/
+
+	//Each and every collector must implement the Describe function.
+//It essentially writes all descriptors to the prometheus desc channel.
+	func (collector *fooCollector) Describe(ch chan<- *prometheus.Desc) {
+
+		//Update this section with the each metric you create for a given collector
+		ch <- collector.IncomingCallaccepts
+		ch <- collector.IncomingCallattempts
+	}
+	//Collect implements required collect function for all promehteus collectors
+
+	func (collector *fooCollector) Collect(ch chan<- prometheus.Metric) {
+
+		//Implement logic here to determine proper metric value to return to prometheus
+		//for each descriptor or call other functions that do so.
+		var metricValue1 float64
+
+		data, _ := ioutil.ReadFile("APIoutput.xml")
+		sbc := &SBCdata{}
+		xml.Unmarshal([]byte(data), &sbc)
+		fmt.Println("SBC router ID: ",sbc.Isdnsg.Id, "\nRouter href: ",sbc.Isdnsg.Href)
+		//var metricValue2 float64
+		
+		if s, err := strconv.ParseFloat(sbc.Isdnsg.Id, 64); err == nil {
+			metricValue1 = s
+			fmt.Println(s) // 3.1415927410125732
+		}
+		
+
+		//Write latest value for each metric in the prometheus metric channel.
+		//Note that you can pass CounterValue, GaugeValue, or UntypedValue types here.
+		m1 := prometheus.MustNewConstMetric(collector.IncomingCallattempts, prometheus.GaugeValue, metricValue1)
+		m2 := prometheus.MustNewConstMetric(collector.IncomingCallaccepts, prometheus.GaugeValue, metricValue1)
+		m1 = prometheus.NewMetricWithTimestamp(time.Now().Add(-time.Hour), m1)
+		m2 = prometheus.NewMetricWithTimestamp(time.Now(), m2)
+		ch <- m1
+		ch <- m2
+	}
+
 func main() {
 
-	/* var (
-		targetHost = flag.String("target.host", "localhost", "nginx address with basic_status page")
-		targetPort = flag.Int("target.port", 8080, "nginx port with basic_status page")
-		targetPath = flag.String("target.path", "/status", "URL path to scrap metrics")
-		promPort   = flag.Int("prom.port", 9150, "port to expose prometheus metrics")
-		logPath    = flag.String("target.log", "/var/log/nginx/access.log", "path to access.log")
-	)
-	flag.Parse() */
+	foo := newFooCollector()
+	prometheus.MustRegister(foo)
 
-	data, _ := ioutil.ReadFile("APIoutput.xml")
-
-	sbc := &SBCdata{}
-
-	xml.Unmarshal([]byte(data), &sbc)
-
-	fmt.Println("SBC router ID: ",sbc.Isdnsg.Id, "\nRouter href: ",sbc.Isdnsg.Href)
-	//fmt.Println()
-	//fmt.Println(note.From)
-	//fmt.Println(note.Heading)
-	//fmt.Println(note.Body)
+	http.Handle("/console/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":9101", nil))
+	
 }
