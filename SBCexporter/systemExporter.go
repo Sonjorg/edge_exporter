5b1227eea07b78a915c5f644d6f2fcefd071080e
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-03-05 21:23:27 +0100

    test

--- SBCexporter/systemExporter.go
+++ SBCexporter/systemExporter.go
@@ -178,7 +178,7 @@ func sysCollector(collector *sMetrics)  []prometheus.Metric {//(ch chan<- promet
 	password = "PanneKake23"
 	var err error
 	m := []prometheus.Metric{}
-	for i := 0; i < len(m); i++ {
+	for i := 0; i < len(ipaddresses); i++ {
 		fmt.Println(ipaddresses[i])
 
 	//for i := range ipaddresses  {
