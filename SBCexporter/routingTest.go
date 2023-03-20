0babd45c5e00ea516e952734da9cad28e805bbf6
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-03-20 19:59:42 +0100

    routingentry test

---
+++ SBCexporter/routingTest.go
@@ -0,0 +1,123 @@
+//first request
+package main
+import (
+	"encoding/xml"
+	//"log"
+	//"github.com/prometheus/client_golang/prometheus"
+	//"strconv"
+	//"time"
+	//"crypto/tls"
+	"fmt"
+	//"strings"
+	//"io/ioutil"
+	//"net/http"
+	//"net/url"
+	//"time"
+	//"log"
+	"regexp"
+
+
+)
+type rt struct {
+   // Value  float32 `xml:",chardata"`
+	XMLName xml.Name `xml:"root"`
+	Rt2     rt2      `xml:"routingtable_list"`
+}
+type rt2 struct {
+	Rt3    rt3       `xml:"routingtable_pk"`
+	//Value  float32 `xml:",chardata"`
+	//Id         []int `xml:"id,attr"`//`xml:"_pk,attr id="2" href="https://10.233.230.11/rest/routingtable//2"/>
+	//<_pk id="4" href="https://10.233.230.11/rest/routingtable//4"/>
+}
+type rt3 struct {
+	Attr    []string `xml:"id,attr"`
+	Value     string `xml:",chardata"`
+}
+ //Second request
+ type call2xml1 struct {
+	XMLName    xml.Name  `xml:"root"`
+	Call2xml2  call2xml2 `xml:"routingentry_list"`
+ }
+ type call2xml2 struct {
+	Call2xml3  call2xml3 `xml:"routingentry_pk"`
+ }
+ type call2xml3 struct {
+	Attr    []string `xml:"id,attr"`
+	Value     string `xml:",chardata"`
+ }
+
+ type rSBCdata struct {
+	XMLname    xml.Name   `xml:"root"`
+	Status     rStatus    `xml:"status"`
+	routingData routingData `xml:"historicalstatistics"`
+}
+type rStatus struct {
+	HTTPcode string `xml:"http_code"`
+}
+type routingData struct {
+Href                string `xml:"href,attr"`
+Rt_RuleUsage		int    `xml:"rt_RuleUsage"`
+Rt_ASR				int    `xml:"rt_ASR"`
+Rt_RoundTripDelay	int    `xml:"rt_RoundTripDelay"`
+Rt_Jitter           int    `xml:"rt_Jitter"`
+Rt_MOS              int    `xml:"rt_MOS"`
+Rt_QualityFailed    int    `xml:"rt_QualityFailed"`
+}
+
+//func routingCollector(ip string)([]prometheus.Metric, string){
+	func test() {
+	hosts := getIncludedHosts("routingentry")//retrieving targets for this exporter
+	if (len(hosts) <= 0) {
+		return
+	}
+	for i := range hosts {
+
+	phpsessid, err := APISessionAuth("student", "PanneKake23","https://"+hosts[i].ip+"/rest/login")
+	if err != nil {
+		//return nil, err.Error()
+	}
+	data,err := getAPIData("https://"+hosts[i].ip+"/rest/routingtable", phpsessid)
+	if err != nil {
+		//return nil, err.Error()
+	}
+	b := []byte(data) //Converting string of data to bytestream
+	ssbc := &rt{}
+	xml.Unmarshal(b, &ssbc) //Converting XML data to variables
+	//fmt.Println("Successful API call data: ",ssbc.Rt2.Rt3.Attr)
+	routingTables := ssbc.Rt2.Rt3.Attr
+
+	if (len(routingTables) <= 0) {
+		//return nil, "Routingtables empty"
+		fmt.Println("Routingtables empty")
+
+	}
+		for j := range routingTables {
+			url := "https://"+hosts[i].ip+"/rest/routingtable/" + routingTables[j] + "/routingentry"
+			data2, err := getAPIData(url, phpsessid)
+			if err != nil {
+			}
+			b2 := []byte(data2) //Converting string of data to bytestream
+			ssbc2 := &call2xml1{}
+			xml.Unmarshal(b2, &ssbc2) //Converting XML data to variables
+			routingEntries := ssbc2.Call2xml2.Call2xml3.Attr
+
+			if (len(routingEntries) <= 0) {
+				continue
+			}
+			entries := regexp.MustCompile(`\d+$`)
+			fmt.Println("Table:", routingEntries[j])
+
+			for k := range routingEntries {
+				match := entries.FindStringSubmatch(routingEntries[k])
+				fmt.Println(match)
+				url := "https://"+hosts[i].ip+"/rest/routingtable/"+routingTables[j]+"/routingentry/"+routingEntries[k]+"/historicalstatistics/1"
+				data3, err := getAPIData(url, phpsessid)
+					if err != nil {
+					}
+				fmt.Println(data3)
+			}
+		}
+
+	}
+}
+
