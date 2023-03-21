5f877f53dc8a288266e250650cb48b07796ef1ac
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-03-21 01:59:36 +0100

    json test

--- SBCexporter/routingentryExporter.go
+++ SBCexporter/routingentryExporter.go
@@ -191,7 +191,7 @@ func (collector *rMetrics) Collect(c chan<- prometheus.Metric) {
 				}
 
 				for k := range match {
-					phpsessid,err := APISessionAuth("student", "PanneKake23","https://"+hosts[i].ip+"/rest/login")
+					phpsessid,err := APISessionAuth("student", "PanneKake23",hosts[i].ip)
 						if err != nil {
 							fmt.Println(err)
 							continue
