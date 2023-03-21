7232a89e4b51d64b34f462a4602a77b7b417a76d
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-03-21 01:34:59 +0100

    json test

--- SBCexporter/apicall.go
+++ SBCexporter/apicall.go
@@ -37,13 +37,14 @@ func APISessionAuth(username string, password string, ipaddress string) (string,
 	}
 //Checks if current time is 10 min after logged time in json file
 //If so, use the sessioncookie stored in the json file
+/*
 	if (time.Now().After(Hosts.Time.Add(1 * time.Minute))){ //Hosts.Time.After(time.Now().Add(1 * time.Minute))) {
 		if (Hosts.Ipaddress == ipaddress) {
 			//fmt.Println("retrieved from file")
 			return Hosts.Phpsessid, err
 		}
 	}
-
+*/
 
 	cfg := getConf(&Config{})
 	timeout := cfg.Authtimeout
