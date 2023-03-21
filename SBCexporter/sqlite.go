49b0a5ae3cbe5e8369a385d1267308378bcb7f98
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-03-21 23:06:30 +0100

    sqlite test

---
+++ SBCexporter/sqlite.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"github.com/mattn/go-sqlite3" // Import go-sqlite3 library
+	"fmt"
+)
+
+func createTable(db *sql.DB) {
+	createAuthTableSQL := `CREATE TABLE authentication (
+		"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"ipaddress" TEXT,
+		"phpsessid" TEXT,
+		"time" TEXT
+	  );` // SQL Statement for Create Table
+
+	log.Println("Create table...")
+	statement, err := db.Prepare(createAuthTableSQL) // Prepare SQL Statement
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec() // Execute SQL Statements
+	log.Println("table created")
+}
+
+// We are passing db reference connection from main to our method with other parameters
+func insertAuth(db *sql.DB, ipaddress string, phpsessid string, time string) {
+	log.Println("Inserting record ...")
+	insertAuthSQL := `INSERT INTO authentication(ipaddress, phpsessid, time) VALUES (?, ?, ?)`
+	statement, err := db.Prepare(insertAuthSQL) // Prepare statement.
+                                                   // This is good to avoid SQL injections
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	_, err = statement.Exec(ipaddress, phpsessid, time)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func displayAuth(db *sql.DB) (ipaddress string, phpsessid string, time string){
+	row, err := db.Query("SELECT * FROM authentication ORDER BY time")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		var id int
+		var ipaddress string
+		var phpsessid string
+		var time string
+		row.Scan(&id, &ipaddress, &phpsessid, &time)
+		//log.Println("Student: ", code, " ", name, " ", program)
+	}
+	return ipaddress, phpsessid, time
+
+}
