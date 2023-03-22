e67cac41d6e81ab4305852246af7b017ebf9ba44
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-03-22 21:17:30 +0100

    sqlite test

--- SBCexporter/apicall.go
+++ SBCexporter/apicall.go
@@ -88,6 +88,7 @@ func APISessionAuth(username string, password string, ipaddress string) (string,
 	if err != nil {
 		fmt.Println("kan ikke kaste table",err)
 	}*/
+	err = dropTable(sqliteDatabase)
 	err = createTable(sqliteDatabase)
 	if err != nil {
 		fmt.Println("kan ikke lage table")
