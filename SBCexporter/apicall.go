cedc10c4bca281840cc951712b61d031bf38e33e
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-03-22 20:07:11 +0100

    sqlite test

--- SBCexporter/apicall.go
+++ SBCexporter/apicall.go
@@ -128,9 +128,11 @@ func getSqliteData(ipaddress string) (cookie string, err error){
 	for i:= range Hosts {
 		fmt.Println(Hosts[i].Ipaddress)
 		if (Hosts[i].Ipaddress == ipaddress) {
-			timeLast,_ := time.Parse(Hosts[i].Time, Hosts[i].Time)
+			timeLast,_ := time.Parse(time.Now().String(), Hosts[i].Time)
 			fmt.Println(timeLast)
+			fmt.Println(time.Now())
 			fmt.Println(timeLast.Add(mins).Before(time.Now()))
+
 			if (timeLast.Add(mins).Before(time.Now()) == true) {
 				//return Hosts[i].Phpsessid,nil
 				//phpsessid = Hosts[i].Phpsessid
