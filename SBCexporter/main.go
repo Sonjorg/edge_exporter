f041f886b81408f87de2a65b719745b59b951e78
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-03-29 22:41:09 +0200

    systemcallstats collector test

--- SBCexporter/main.go
+++ SBCexporter/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 
-	"github.com/prometheus/client_golang/prometheus"
+	//"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
