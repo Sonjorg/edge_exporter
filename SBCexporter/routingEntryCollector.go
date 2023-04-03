915999a0af8a5091f2bab7c9dcd658c85854654f
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-04-03 21:43:04 +0200

    sqlite test

--- SBCexporter/routingEntryCollector.go
+++ SBCexporter/routingEntryCollector.go
@@ -189,7 +189,8 @@ func (collector *rMetrics) Collect(c chan<- prometheus.Metric) {
 				var match []string
 
 				for k := range routingEntries {
-					match = append(match, entries.FindStringSubmatch(routingEntries[k]))
+					match = entries.FindStringSubmatch(routingEntries[k])
+					fmt.Println(entries.FindStringSubmatch(routingEntries[k]))
 				}
 				fmt.Println(routingEntries)
 				fmt.Println(match)
