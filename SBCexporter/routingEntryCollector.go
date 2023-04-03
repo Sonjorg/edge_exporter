439bedb6068a65d112c89bea93ab48df5fa26d9d
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-04-03 22:01:33 +0200

    sqlite test

--- SBCexporter/routingEntryCollector.go
+++ SBCexporter/routingEntryCollector.go
@@ -211,7 +211,7 @@ func (collector *rMetrics) Collect(c chan<- prometheus.Metric) {
 				}
 				createRoutingSqlite(sqliteDatabase)
 				storeRoutingEntries(sqliteDatabase, hosts[i].ip, "time", routingtables[j], match)
-				match ,err = getRoutingEntries(sqliteDatabase,hosts[i].ip,routingtables[j])
+				match ,err := getRoutingEntries(sqliteDatabase,hosts[i].ip,routingtables[j])
 				if err != nil {
 					fmt.Println(err)
 				}
