a47d85695c061d6c0712868b69ecca27b9232d59
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-04-10 18:29:52 +0200

    sql test

--- pkg/database/sqliteRouting.go
+++ pkg/database/sqliteRouting.go
@@ -70,9 +70,10 @@ func StoreRoutingEntries(db *sql.DB, ipaddress string, time string, routingTable
 	return nil
 }
 
-func RoutingTablesExists(db * sql.DB, ip string) bool {
-    sqlStmt := `SELECT ipaddress FROM routingtables WHERE ipaddress = ?`
-    err := db.QueryRow(sqlStmt, ip).Scan(&ip)
+func RoutingTablesExists(db * sql.DB) bool {
+   // sqlStmt := `SELECT ipaddress FROM routingtables WHERE ipaddress = ?`
+	sqlStmt := `SELECT ipaddress FROM sqlite_master WHERE name='{table_name}`
+    err := db.QueryRow(sqlStmt, "routingtables").Scan()
     if err != nil {
         if err != sql.ErrNoRows {
             // a real error happened! you should change your function return
