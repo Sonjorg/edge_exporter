5d9952396235284fe1f22bcb045990fab0ec5305
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-04-12 20:20:08 +0200

    test for chassis labels

---
+++ pkg/database/chassisDB.go
@@ -0,0 +1,75 @@
+package database
+
+import (
+	//"encoding/xml"
+	//"fmt"
+	"log"
+	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
+	"database/sql"
+	//"edge_exporter/pkg/http"
+)
+
+type Chassis struct {
+	Id           int
+	Ipaddress    string
+	ChassisType  string
+	SerialNumber string
+}
+
+func CreateChassis(db *sql.DB) error {
+	createAuthTableSQL := `CREATE TABLE IF NOT EXISTS chassis (
+		"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"ipaddress" TEXT,
+		"chassistype" TEXT,
+		"serialnumber" TEXT
+	  );` // SQL Statement for Create Table
+
+	//log.Println("Create table...")
+	statement, err := db.Prepare(createAuthTableSQL) // Prepare SQL Statement
+	if err != nil {
+		return err
+	}
+	statement.Exec() // Execute SQL Statements
+	//log.Println("table created")
+	return nil
+}
+
+func InsertChassis(db *sql.DB, ipaddress string, chassisType string, serialNumber string) error{
+	log.Println("Inserting session Chassis data ...")
+	insertAuthSQL := `INSERT INTO chassis(ipaddress, chassistype, serialnumber) VALUES (?, ?, ?)`
+
+	statement, err := db.Prepare(insertAuthSQL) // Prepare statement.
+                                                   // This is good to avoid SQL injections
+	if err != nil {
+		return err
+	}
+	_, err = statement.Exec(ipaddress, chassisType, serialNumber)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetChassis(db *sql.DB, ipaddress string) (*Chassis, error){
+	row, err := db.Query("SELECT * FROM chassis WHERE ipaddress = ?", ipaddress)
+	//row.Scan(ip)
+	if err != nil {
+		return nil, err
+		//fmt.Println(err)
+	}
+	defer row.Close()
+
+	var c *Chassis
+	for row.Next() {
+			p := &Chassis{}
+			if err := row.Scan(&p.Id, &p.Ipaddress, &p.ChassisType, &p.SerialNumber); err != nil{
+				 //fmt.Println(err)
+			}
+			if (p.Ipaddress == ipaddress) {
+				c.ChassisType, c.SerialNumber = p.ChassisType, p.SerialNumber
+			}
+	}
+
+	return c, err
+}
+
---
+++ pkg/utils/chassis.go
@@ -0,0 +1,65 @@
+package utils
+
+//system status exporter
+//rest/system/historicalstatistics/1
+
+import (
+	"encoding/xml"
+	"fmt"
+	"log"
+	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
+	"database/sql"
+	"edge_exporter/pkg/database"
+	"edge_exporter/pkg/http"
+)
+
+type ChassisData struct {
+	XMLname    xml.Name   `xml:"root"`
+	Chassis chassis       `xml:"chassis"`
+}
+
+type chassis struct {
+	Rt_Chassis_Type   string `xml:"rt_Chassis_Type"`
+	SerialNumber      string `xml:"SerialNumber"`    // Average percent usage of the CPU.
+}
+
+
+//Collect implements required collect function for all promehteus collectors
+func GetChassisLabels(ipaddress string, phpsessid string) (chassisType string, serialNumber string, err error){
+	//hosts := config.GetAllHosts()//retrieving targets for this exporter
+
+	//fmt.Println(hosts)
+	var sqliteDatabase *sql.DB
+
+	sqliteDatabase, err = sql.Open("sqlite3", "./sqlite-database.db")
+	if err != nil {
+		fmt.Println(err)
+		return "","", err
+	} // Open the created SQLite File
+	// Defer Closing the database
+	defer sqliteDatabase.Close()
+
+	labels, err := database.GetChassis(sqliteDatabase, ipaddress)
+	if err != nil {
+
+		dataStr := "https://"+ipaddress+"/rest/chassis"
+		_, data,err := http.GetAPIData(dataStr, phpsessid)
+		if err != nil {
+				fmt.Println("Error collecting from : ",log.Flags(), err,"\n")
+
+		}
+		b := []byte(data) //Converting string of data to bytestream
+		ssbc := &ChassisData{}
+		xml.Unmarshal(b, &ssbc) //Converting XML data to variables
+		//fmt.Println("Successful API call data: ",ssbc.SystemData,"\n")
+
+		chassisType := ssbc.Chassis.Rt_Chassis_Type
+		serialNumber := ssbc.Chassis.SerialNumber
+		err = database.InsertChassis(sqliteDatabase, ipaddress, chassisType, serialNumber)
+			if err != nil {
+				log.Println(err)
+			}
+		return chassisType, serialNumber, nil
+	}
+return labels.ChassisType, labels.SerialNumber, err
+}
