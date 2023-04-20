a22803b1cd3f3d4c8c1d6e954d02521e4eadc272
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-04-20 15:12:34 +0200

    replace db test

--- pkg/utils/reusableData.go
+++ pkg/utils/reusableData.go
@@ -1,6 +1,6 @@
 package utils
 
-import "fmt"
+//import "fmt"
 
 //"github.com/mattn/go-sqlite3" // Import go-sqlite3 library
 //_ "github.com/mattn/go-sqlite3"
@@ -10,40 +10,56 @@ type RoutingTablesUtils struct {
 	Id                int
 	Time              string
 	RoutingTables   []string
+	Ipaddress string
 	RoutingEntries  map[string][]string
 
 	 //map consisting of routingtables and their routingentries
 }
 type RoutingData struct {
-	Routing  map[string]RoutingTablesUtils
+	Ipaddress string
+	Routing  RoutingTablesUtils
 }
 func StoreRoutingEntries(ipaddress string, time string, routingTable string, routingEntries []string) {
 
 	m :=  make(map[string][]string)
 	m2 :=  make(map[string]RoutingTablesUtils)
 
-	s := &RoutingData{}
-	t := &RoutingTablesUtils{}
+	s := []RoutingTablesUtils{}
+	t := RoutingTablesUtils{}
 	t.Time = time
 
 	m[routingTable] = routingEntries
 
+	a := RoutingTablesUtils{Ipaddress: ipaddress, Time: time, RoutingEntries: m}
 	t.RoutingEntries = m
 
 	t.RoutingTables = append(t.RoutingTables, routingTable)
-	m2[ipaddress] = *t
-	s.Routing = m2
+	m2[ipaddress] = t
+	//Ipaddress := ipaddress
+
+		s = append(s, a)
+		//	s.Routing = append(s.Routing,t)
+
+
+
 
 }
 
 func GetRoutingData(ipaddress string, r RoutingData) (map[string][]string,[]string,string) {
 
-	fmt.Println(r.Routing[ipaddress].RoutingEntries, r.Routing[ipaddress].RoutingTables,r.Routing[ipaddress].Time)
+	//fmt.Println(r.Routing[ipaddress].RoutingEntries, r.Routing[ipaddress].RoutingTables,r.Routing[ipaddress].Time)
 
-	_, ok := r.Routing[ipaddress]
+	/*_, ok := r.Routing[ipaddress]
 	if !ok {
 		return nil,nil,	"no data"
-	}
-	return r.Routing[ipaddress].RoutingEntries, r.Routing[ipaddress].RoutingTables,r.Routing[ipaddress].Time
+	}*/
+	s := []RoutingTablesUtils{}
+	for i:= range s {
+		if (s[i].Ipaddress == ipaddress){
+			return s[i].RoutingEntries, s[i].RoutingTables,s[i].Time
+		}
 
+	}
+
+	return nil,nil,"no data"
 }
