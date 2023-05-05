f51fb7859c68e9c2be1b0b41d87116cf602845b9
Sondre Jørgensen <sondrjor@stud.ntnu.no>
2023-05-05 20:29:31 +0200

    new design test

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"edge_exporter/pkg/collector"
+	"edge_exporter/pkg/config"
+	"edge_exporter/pkg/database"
+	"edge_exporter/pkg/utils"
+	thishttp "edge_exporter/pkg/http"
+	//"fmt"
+	"log"
+	"net/http"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func main() {
+	//Creating database and tables
+	database.InitializeDB()
+
+	hosts := config.GetAllHosts()
+	for i := range hosts {
+		//Fetching sessioncookies and inserting them into the database
+		phpsessid, err := thishttp.APISessionAuth(hosts[i].Username, hosts[i].Password, hosts[i].Ip)
+		if err!= nil {
+			log.Print(err)
+			continue
+		}
+		//Fetching SBC type and serialnumbers, and placing them in database
+		_, _, err = utils.GetChassisLabels(hosts[i].Ip, phpsessid)
+		if err!= nil {
+			log.Print(err)
+			continue
+		}
+	}
+
+	//initializing collectors
+	//The collectors only start to run after a scrape is made to this host
+	//collector.SystemResourceCollector()
+	//collector.DiskPartitionCollector()
+	//collector.RoutingEntryCollector()
+	//collector.CallStatsCollector()
+	//collector.LinecardCollector()
+	//collector.EthernetportCollector()
+	registry := prometheus.NewRegistry()
+	c := &collector.AllCollectors{}
+	registry.MustRegister(c)
+		c.Probe()
+
+	//Serving metrics
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":1234", nil))
+
+}
---
+++ pkg/collector/probe.go
@@ -0,0 +1,34 @@
+package collector
+
+import (
+	//"fmt"
+	//"edge_exporter/pkg/collector"
+	"github.com/prometheus/client_golang/prometheus"
+	//"strconv"
+	//"time"
+)
+
+type AllCollectors struct{
+	metrics []prometheus.Metric
+}
+
+func (m *AllCollectors) Probe() {
+	metrics := LinecardCollector2()
+	for i := range metrics {
+		m.metrics= append(m.metrics, metrics[i])
+	}
+}
+
+
+func (collector *AllCollectors) Collect(c chan<- prometheus.Metric) {
+	for _, m := range collector.metrics {
+		c <- m
+	}
+}
+func (collector *AllCollectors) Describe(ch chan<- *prometheus.Desc) {
+	//Update this section with the each metric you create for a given collector
+	//ch <- collector.Rt_CardType
+	//ch <- collector.Rt_Location
+	
+	//ch <- collector.Error_ip
+}
